d5949a4a49f7caf63e7389283d1656b8ab8f7dd9
折田幸夫 <3204704@gmail.com>
2020-02-29 17:16:21 +0900

    feat port

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.get("/", (res, req) => {
   res.send("Hello CRUD!!!");
 });
 
-app.listen(3001, () => {
+//実際にwebで叩く場合うまくいかな場合があるのでportをprocess.env.PORTで行う。
+app.listen(port, () => {
   //serverが立ち上がった時にする処理
-  console.log("ポート番号3001で立ち上がりました。");
+  console.log(`ポート番号${port}で立ち上がりました。`);
 });
