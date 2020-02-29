6af50914c61953f4f078e05e37a48f4530c388ee
折田幸夫 <3204704@gmail.com>
2020-02-29 17:45:25 +0900

    feat query

--- App.js
+++ App.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (res, req) => {
-  res.send("Hello CRUD!!!");
+app.get("/", (req, res) => {
+  res.send("Hello CRUD!");
+});
+
+app.get("/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/:year/:month", (req, res) => {
+  res.send(`${req.params.year}年の${req.params.month}月の記事です。`);
+});
+
+app.get("/jsonGet", (req, res) => {
+  //queryStringもjson形式で取得できる
+  res.send(req.query);
 });
 
 //実際にwebで叩く場合うまくいかな場合があるのでportをprocess.env.PORTで行う。
